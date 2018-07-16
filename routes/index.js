c97b4f524b08447dc9285b335e51334c9f4b5113
“yannyyss” <yannyyss@hotmail.com>
2018-07-16 12:26:21 -0500

    nav bar en proceso

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router  = express.Router();
 
+/* MiddleWheres */
+
+/*
+const splash = (req, res, next)=> {
+  res.render("splash");
+  setTimeout(function () {    
+    res.redirect("index");
+  }, 3000);
+  //
+}*/
+
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', /* splash,*/ (req, res, next) => {
   res.render('index');
 });
 
