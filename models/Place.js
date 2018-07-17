c32675a3efa5f41b7498ff27ff223494f07ac697
“yannyyss” <yannyyss@hotmail.com>
2018-07-17 12:27:09 -0500

    map con auto completar

---
+++ models/Place.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema({
+    tipodelugar:{
+        type: String,
+        enum: ["Robo","Acoso Sexual","Grupos Sospechosos","Enfrentamientos Armados","Sin Iluminación","Agresión"],
+    },
+    location: {
+        type: {
+            type: String,
+            default: "Point"
+        },
+        address: String,
+        coordinates: [
+            {
+                type: Number
+            }
+        ]
+    }
+},{
+    timestamps: {
+        createdAt: "created_at",
+        updatedAt: "updated_at"
+    }
+});
+
+module.exports = mongoose.model('Place', placeSchema);
