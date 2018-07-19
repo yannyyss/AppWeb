c916f14b0e119203f9351a65f8cfb4586f23916c
nallelyht <nallelyht@gmail.com>
2018-07-19 17:55:25 -0500

    comentarios anonimos

--- routes/places.js
+++ routes/places.js
@@ -15,30 +15,42 @@ function isLoggedIn(req, res, next) {
 }
 /* map */
 
-router.get('/',isLoggedIn,(req,res)=>{
-    res.render('map');
-  });
-
-router.post('/new', isLoggedIn, (req, res, next) => {
-	req.body.user = req.user._id;
-	Place.create(req.body)
-		.then((place) => {
-            console.log(place)
-			User.findByIdAndUpdate(req.user._id, { $push: { places: place._id } },{ new: true }).then((user)=>{console.log(user)}).catch((e) => next(e));
-			res.redirect('/map');
-		})
-		.catch((e) => next(e));
+router.get('/', isLoggedIn, (req, res) => {
+	res.render('map');
 });
 
-router.get('/test', (req,res)=>{
-	if(Object.keys(req.query).length > 0){
+router.post('/new', (req, res, next) => {
+	if (req.user) {
+		req.body.user = req.user._id;
+		Place.create(req.body)
+			.then((place) => {
+				User.findByIdAndUpdate(req.user._id, { $push: { places: place._id } }, { new: true })
+					.then((user) => {
+						console.log(user);
+					})
+					.catch((e) => next(e));
+				res.redirect('/map');
+			})
+			.catch((e) => next(e));
+	} else {
+		Place.create(req.body)
+			.then(() => {
+				res.redirect('/map');
+			})
+			.catch((e) => next(e));
+	}
+});
+
+router.get('/test', (req, res) => {
+	if (Object.keys(req.query).length > 0) {
 		console.log(Object.keys(req.query)); //imprime la llave de query
-		Place.find({tipodelugar: { $in: Object.keys(req.query) } //Busca en todos las instacias que se han creado con el modelo Place, el query que conincida con la key
-		}).then(items => {
-		res.json(items); //muestra un json en el navegador
+		Place.find({
+			tipodelugar: { $in: Object.keys(req.query) } //Busca en todos las instacias que se han creado con el modelo Place, el query que conincida con la key
+		}).then((items) => {
+			res.json(items); //muestra un json en el navegador
 		});
 	}
 	//res.send("no hay querys mijo")
-})
+});
 
 module.exports = router;
